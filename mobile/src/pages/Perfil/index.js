33899ce20deb63a43298798ed394a073e8785d75
Danilo J <danilospinfo@protonmail.com>
2020-06-12 17:50:06 -0300

    add login screen [not working fully yet]

--- mobile/src/pages/Perfil/index.js
+++ mobile/src/pages/Perfil/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import Container from '~/components/Container';
-import { Area, Title } from './styles';
+import {Area, InputRow, Title, DefaultInput, PasswordInput} from './styles';
+import { AntDesign } from '@expo/vector-icons';
 
 const Cronograma = () => {
   return (
     <Container>
       <Area>
-        <Title>Perfil</Title>
+        <Title>
+          Crie uma conta para você.
+        </Title>
+        <InputRow>
+          <DefaultInput 
+          placeholder = {'Nome'}
+          />
+        </InputRow>
+        <InputRow>
+          <AntDesign name="mail" 
+          size={24} color="black"
+          />
+          <DefaultInput
+           placeholder={'Email'}
+           />
+        </InputRow>
+        <InputRow>
+          <AntDesign name="mail" 
+          size={24} color="black"
+          />
+          <DefaultInput
+           placeholder={'Confirmar email'}
+          />
+        </InputRow>
+        <InputRow>
+          <AntDesign name="lock"
+          size={24} color="black"
+          />
+          <PasswordInput 
+          placeholder={'Senha'}
+          />
+        </InputRow>
+        <InputRow>
+          <PasswordInput 
+          placeholder={'Confirmar senha'}
+          />
+        </InputRow>
       </Area>
     </Container>
   );
--- mobile/src/pages/Perfil/styles.js
+++ mobile/src/pages/Perfil/styles.js
@@ -4,9 +4,27 @@ export const Area = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: rgba(255, 0, 0, 0.39);
-`;
+  background-color: gray;
+`
+
+export const InputRow = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-context: space-between;
+`
 
 export const Title = styled.Text`
   font-size: 32px;
-`;
+`
+
+export const DefaultInput = styled.TextInput`
+  height: 40;
+  font-size: 16;
+`
+
+export const PasswordInput = styled.TextInput`
+  height: 40;
+  font-size: 16;
+`
+
+
