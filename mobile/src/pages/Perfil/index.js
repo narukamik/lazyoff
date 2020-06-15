a014bf08d84f3af95291a160482cbaec99d11c31
Danilo J <danilospinfo@protonmail.com>
2020-06-15 13:31:38 -0300

    password input is now set as a password field

--- mobile/src/pages/Perfil/index.js
+++ mobile/src/pages/Perfil/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Container from '~/components/Container';
-import { Area, InputRow, Icon, Title, DefaultInput, PasswordInput } from './styles';
+import { 
+  Area,
+  InputRow,
+  Icon,
+  Title,
+  DefaultInput,
+  PasswordInput
+} from './styles';
 
 const Cronograma = () => {
   return (
@@ -8,24 +15,30 @@ const Cronograma = () => {
       <Area>
         <Title>Crie uma conta para você.</Title>
         <InputRow>
-          <Icon name="user" size={24} color="black" />
+          <Icon name="user" size={24} color="white" />
           <DefaultInput placeholder="Nome" />
         </InputRow>
         <InputRow>
-          <Icon name="mail" size={24} color="black" />
+          <Icon name="mail" size={24} color="white" />
           <DefaultInput placeholder="Email" />
         </InputRow>
         <InputRow>
-          <Icon name="mail" size={24} color="black" />
+          <Icon name="mail" size={24} color="white" />
           <DefaultInput placeholder="Confirmar email" />
         </InputRow>
         <InputRow>
-          <Icon name="lock" size={24} color="black" />
-          <PasswordInput placeholder="Senha" />
+          <Icon name="lock" size={24} color="white" />
+          <PasswordInput
+          secureTextEntry={true}
+          password={true}
+          placeholder="Senha" />
         </InputRow>
         <InputRow>
           <Icon name="lock" size={24} color="black" />
-          <PasswordInput placeholder="Confirmar senha" />
+          <PasswordInput 
+          secureTextEntry={true}
+          password={true}
+          placeholder="Confirmar senha" />
         </InputRow>
       </Area>
     </Container>
