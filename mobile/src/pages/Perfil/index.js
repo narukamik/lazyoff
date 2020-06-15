548007cd969d2eebe1728a852af1dbbe7128e6d2
Danilo J <danilospinfo@protonmail.com>
2020-06-15 12:27:47 -0300

    signup icons working again

--- mobile/src/pages/Perfil/index.js
+++ mobile/src/pages/Perfil/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { AntDesign, Ionicons } from '@expo/vector-icons';
 import Container from '~/components/Container';
 import { Area, InputRow, Icon, Title, DefaultInput, PasswordInput } from './styles';
 
@@ -9,7 +8,7 @@ const Cronograma = () => {
       <Area>
         <Title>Crie uma conta para você.</Title>
         <InputRow>
-          <Icon name="md-person" size={24} color="black" />
+          <Icon name="user" size={24} color="black" />
           <DefaultInput placeholder="Nome" />
         </InputRow>
         <InputRow>
@@ -25,7 +24,7 @@ const Cronograma = () => {
           <PasswordInput placeholder="Senha" />
         </InputRow>
         <InputRow>
-          <iconSet name="lock" size={24} color="black" />
+          <Icon name="lock" size={24} color="black" />
           <PasswordInput placeholder="Confirmar senha" />
         </InputRow>
       </Area>
--- mobile/src/pages/Perfil/styles.js
+++ mobile/src/pages/Perfil/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
 
 export const Area = styled.View`
   flex: 1;
