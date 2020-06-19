800c4daf61b38891e23ed352b3dd1955c96c2fa9
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-06-19 19:09:33 -0300

    Checklist page

--- mobile/src/pages/Checklist/index.js
+++ mobile/src/pages/Checklist/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useState } from 'react';
 import Container from '~/components/Container';
-import CronHeader from '~/components/CronHeader';
-import {  Button } from './styles';
+import { CheckBox} from "react-native";
+import {
+  Area,
+  CheckArea,
+  CheckText,
+  CheckItem,
+  Text,
+  Title,
+  HeaderTop,
+  Div,
+} from './styles';
 
 const Checklist = () => {
+
+  const [isSelected, setSelection] = useState(false);
+
   return (
     <Container>
-      <Button>
-      <CronHeader icon="clock"/>
-      </Button>
+      <HeaderTop>
+          <Title>Segunda, 14 de junho</Title>
+      </HeaderTop>
+      <Area>
+        <CheckArea>
+          <CheckItem>
+            <CheckBox value={isSelected} name="Tarefa" onValueChange={setSelection} />
+            <CheckText>Ração do Cachorro</CheckText>
+          </CheckItem>
+          <CheckItem>
+            <CheckBox value={isSelected} name="tarefa" onValueChange={setSelection} />
+            <CheckText>Aniversário do Frodo</CheckText>
+          </CheckItem>
+        </CheckArea>
+        <Text>Comprar produtos de Limpeza (4)</Text>
+        <Div>
+        <CheckArea>
+          <CheckItem>
+            <CheckBox value={isSelected} name="Tarefa" onValueChange={setSelection} />
+            <CheckText>Desidratante</CheckText>
+          </CheckItem>
+          <CheckItem>
+            <CheckBox value={isSelected} name="tarefa" onValueChange={setSelection} />
+            <CheckText>Sabão em pó</CheckText>
+          </CheckItem>
+          <CheckItem>
+            <CheckBox value={isSelected} name="tarefa" onValueChange={setSelection} />
+            <CheckText>Vanish</CheckText>
+          </CheckItem>
+          <CheckItem>
+            <CheckBox value={isSelected} name="tarefa" onValueChange={setSelection} />
+            <CheckText>Vanish</CheckText>
+          </CheckItem>
+        </CheckArea>
+        </Div>
+      </Area>
     </Container>
   );
 };
