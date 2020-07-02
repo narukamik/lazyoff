fd614c145c922c38ac27f2219477f4918664e4f3
Julio Bittenbinder <juliocbittenbinder@gmail.com>
2020-07-02 16:23:07 -0300

    carousel component / profile header fix

--- mobile/src/pages/Profile/styles.js
+++ mobile/src/pages/Profile/styles.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components/native';
 import colors from '~/config/ColorConfig';
 
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #F7FAFC;
+`
+
 export const Area = styled.View`
   width: 100%;
-  height: 100%;
-  background-color: white;
+  background-color: #F7FAFC;
 `;
 
 export const HeaderStatus = styled.View`
@@ -25,7 +29,7 @@ export const Status = styled.View`
 export const Coins = styled.View`
   background-color: gold;
   border-radius: 6px;
-  height: 30px;
+  height: 27px;
   padding: 0 16px;
   justify-content: center;
 `;
@@ -39,17 +43,20 @@ export const Coin = styled.Image`
 
 export const TextCoins = styled.Text`
   color: white;
-  font-size: 20px;
+  font-size: 19px;
   font-weight: bold;
   text-align: center;
+  padding: 0 0 0 10px;
 `;
 
 export const Photo = styled.Image`
-  width: 40px;
-  height: 40px;
+  width: 45px;
+  height: 45px;
   border-radius: 50px;
   border-width: 2px;
   border-color: gold;
+  z-index: 2;
+  margin-right: -10px;
 `;
 
 export const TextLevel = styled.Text`
@@ -57,6 +64,7 @@ export const TextLevel = styled.Text`
   font-size: 12px;
   font-weight: bold;
   text-align: center;
+  padding: 0 0 0 10px;
 `;
 
 export const Level = styled.View`
@@ -83,4 +91,13 @@ export const TextValue = styled.Text`
 export const Trophy = styled.Image`
   width: 30px;
   height: 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: left;
+  margin: 0 16px 16px;
+  color: black;
+  opacity: 0.78;
 `;
