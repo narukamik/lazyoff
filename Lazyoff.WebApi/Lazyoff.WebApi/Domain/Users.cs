3223b0010d84222860d49b5dddeecdea48abbb70
Danilo J <danilo.juniorintelitrader.com.br>
2020-07-10 17:59:24 -0300

    acerta o que falta

---
+++ Lazyoff.WebApi/Lazyoff.WebApi/Domain/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lazyoff.WebApi.Domain
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Coins { get; set; }
+        public int Trophies { get; set; }
+        public int Level { get; set; }
+    }
+}
---
+++ Lazyoff.WebApi/Lazyoff.WebApi/Domain/lazyoffContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Lazyoff.WebApi.Domain
+{
+    public partial class lazyoffContext : DbContext
+    {
+        public lazyoffContext()
+        {
+        }
+
+        public lazyoffContext(DbContextOptions<lazyoffContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=database-1.cxjwhcugdy5z.us-east-2.rds.amazonaws.com;Database=lazyoff;Username=postgres;Password=fA0hBWyYlT1SN2napkf8");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasIdentityOptions(null, null, null, 100000L, null, 1232131L)
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.Email).IsRequired();
+
+                entity.Property(e => e.Name).IsRequired();
+
+                entity.Property(e => e.Password).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
